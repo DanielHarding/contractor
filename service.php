1a3c937063643299e6db840f8b6803454b35906d
DanielHarding <djharding@hotmail.com>
2012-07-22 19:13:16 +0100

    Added service template

---
+++ service.php
@@ -0,0 +1,83 @@
+<?php
+/*
+Template Name: Service Template
+*/
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package Now
+ * @since Now 1.0
+ */
+get_header(); ?>
+
+<div class="row">
+
+
+
+
+  <div class="eight columns">
+
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+      <header class="entry-header">
+        <?php the_post_thumbnail(); ?>
+        <h2 class="entry-title"><?php the_title(); ?></h2>
+      </header><!-- .entry-header -->
+
+      <div class="entry-content">
+      
+        <?php echo $post->post_content; ?>
+
+      </div>
+    </article>
+
+  </div>
+
+
+
+
+  <div class="four columns">
+      <h3>All Services</h3>
+      <ul class="simple-list">
+        <?php 
+          query_posts(array('showposts' => 20, 
+                            // 'post_parent' => $post->ID, 
+                            'order'=>'ASC',
+                            'orderby'=> 'menu_order',
+                            'post_type' => 'service',
+                            'post_status' => 'publish',
+                            'posts_per_page' => 100)); 
+
+          $c=0;
+          while ($p = have_posts()) { 
+            the_post();
+
+            // Remove spaces
+            $ti = get_the_title(get_the_id());
+            $tok = strtolower(str_replace(' ', '-', $ti));
+            $tok = str_replace(':', '', $tok);
+            $tok = str_replace('--', '-', $tok);
+
+            // Set first item as active
+            $klass = ($c == 0) ? 'active' : '';
+          ?>
+
+          <li id="<?php echo $tok; ?>" class="<?php echo $klass; ?>">
+            <?php echo '<a href="' . get_permalink() . '">'.$ti.'</a>'; ?>
+          </li>
+
+        <?php 
+        $c++;
+        } 
+        ?>
+      </ul>
+  </div>
+
+</div>
+
+<?php get_footer(); ?>
