540f66fb010c962896676489465902059f4141c8
DanielHarding <djharding@hotmail.com>
2012-07-22 23:44:35 +0100

    Change columns on single-trade.php

--- single-trade.php
+++ single-trade.php
@@ -11,7 +11,7 @@ get_header(); ?>
 
 <div class="row">
   
-	<div class="eight columns">
+	<div class="nine columns">
 	<?php while ( have_posts() ) : the_post(); ?>
 		<?php // contractor_content_nav( 'nav-above' ); ?>
 		<?php get_template_part( 'content', 'trade' ); ?>
@@ -22,9 +22,12 @@ get_header(); ?>
 
 
 
-	<div class="four columns">
+	<div class="three columns">
+
+
       <h3>All Trades</h3>
-      <ul class="simple-list">
+      
+      <ul class="simple-list ico-list">
         <?php 
           query_posts(array('showposts' => 20, 
                             // 'post_parent' => $post->ID, 
@@ -40,11 +43,16 @@ get_header(); ?>
 
             // Remove spaces
             $ti = get_the_title(get_the_id());
+            $tok = strtolower(str_replace(' ', '-', $ti));
+            $tok = str_replace(':', '', $tok);
+            $tok = str_replace('--', '-', $tok);
+
             // Set first item as active
             $klass = ($c == 0) ? 'active' : '';
           ?>
 
           <li id="<?php echo $tok; ?>" class="<?php echo $klass; ?>">
+            <span class="ico-trade-<?php echo $tok; ?> twenty-four-sq">&nbsp;</span>
             <?php echo '<a href="' . get_permalink() . '">'.$ti.'</a>'; ?>
           </li>
 
@@ -53,6 +61,45 @@ get_header(); ?>
         } 
         ?>
       </ul>
+
+
+      <h3>All Services</h3>
+      <ul class="simple-list ico-list">
+        <?php 
+          query_posts(array('showposts' => 20, 
+                            // 'post_parent' => $post->ID, 
+                            'order'=>'ASC',
+                            'orderby'=> 'menu_order',
+                            'post_type' => 'service',
+                            'post_status' => 'publish',
+                            'posts_per_page' => 100)); 
+
+          $c=0;
+          while ($p = have_posts()) { 
+            the_post();
+
+            // Remove spaces
+            $ti = get_the_title(get_the_id());
+            $tok = strtolower(str_replace(' ', '-', $ti));
+            $tok = str_replace(':', '', $tok);
+            $tok = str_replace('--', '-', $tok);
+
+            // Set first item as active
+            $klass = ($c == 0) ? 'active' : '';
+          ?>
+
+          <li id="<?php echo $tok; ?>" class="<?php echo $klass; ?>">
+            <span class="ico-<?php echo $tok; ?> twenty-four-sq">&nbsp;</span>
+            <?php echo '<a href="' . get_permalink() . '">'.$ti.'</a>'; ?>
+          </li>
+
+        <?php 
+        $c++;
+        } 
+        ?>
+      </ul>
+
+
 	</div>
 
 </div>
